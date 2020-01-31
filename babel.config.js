49edeb174e9e0863685fcec5c6177b4816a3c3dd
huzekang <1040080742@qq.com>
2020-02-01 02:27:19 +0800

    add: 1.在发布阶段移除console

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,10 @@
+// 这是项目发布阶段需要用到的babel插件
+const proPlugins = []
+if (process.env.NODE_ENV === 'production') {
+  // 移除console
+  proPlugins.push('transform-remove-console')
+}
+
 module.exports = {
   'presets': [
     '@vue/app'
@@ -9,6 +16,8 @@ module.exports = {
         'libraryName': 'element-ui',
         'styleLibraryName': 'theme-chalk'
       }
-    ]
+    ],
+    // 发布产品时候的插件数组，...数组中每一项元素都添加进来
+    ...proPlugins
   ]
 }
