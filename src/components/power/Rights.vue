9b7e274b7b3827969b9503a8f97b098f24d1a368
huzekang <1040080742@qq.com>
2020-01-31 22:41:09 +0800

    add:1.路由添加权限列表组件

---
+++ src/components/power/Rights.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h3>权限列表组件页面</h3>
+  </div>
+</template>
+<script>
+  export default {
+    data() {
+      return {}
+    },
+    created() {
+    },
+    methods: {}
+  }
+</script>
+<style lang="less" scoped>
+</style>
--- src/router.js
+++ src/router.js
@@ -4,6 +4,7 @@ import Login from './components/Login.vue'
 import Home from './components/Home.vue'
 import Welcome from './components/Welcome'
 import User from './components/user/Users'
+import Rights from './components/power/Rights'
 
 Vue.use(Router)
 
@@ -17,7 +18,8 @@ const router = new Router({
       redirect: '/welcome',
       children: [
         { path: '/welcome', component: Welcome },
-        { path: '/users', component: User }
+        { path: '/users', component: User },
+        { path: '/rights', component: Rights }
       ]
     }
   ]
