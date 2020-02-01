4427214515f94f2759b76397cad7c3648bdb76d7
huzekang <1040080742@qq.com>
2020-02-01 12:52:29 +0800

    add: 1.路由加载商品分类组件

---
+++ src/components/goods/Cate.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h3>商品分类组件页面</h3>
+  </div>
+</template>
+<script>
+  export default {
+    data() {
+      return {}
+    },
+    created() {
+    },
+    methods: {}
+  }
+</script>
+<style lang="less" scoped>
+</style>
