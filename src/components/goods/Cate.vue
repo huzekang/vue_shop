b5a59b838b1e2dc1e1df0911a31664bd55ce4d83
huzekang <1040080742@qq.com>
2020-02-01 13:09:40 +0800

    add: 1.商品分类列表接口获取

--- src/components/goods/Cate.vue
+++ src/components/goods/Cate.vue
@@ -24,11 +24,34 @@
 <script>
   export default {
     data() {
-      return {}
+      return {
+        // 商品分类列表数据
+        cateList: [],
+        // 查询商品分类列表接口的请求参数
+        queryInfo: {
+          type: 3,
+          pagenum: 1,
+          pagesize: 5
+        },
+        // 总条数
+        totoal: 0
+      }
     },
     created() {
+      this.getCateList()
     },
-    methods: {}
+    methods: {
+      async getCateList() {
+        const { data: res } = await this.$http.get('categories', { params: this.queryInfo })
+        if (res.meta.status !== 200) {
+          return this.$message.error('获取商品列表失败！')
+        }
+        // 把数据列表赋值给cateList
+        this.cateList = res.data
+        // 为总条数赋值
+        this.total = res.data.totoal
+      }
+    }
   }
 </script>
 <style lang="less" scoped>
